b4795a7889cf6dff0982284ff842146c3c1ea586
Andressa Sadia <72982570+Andressa-Sadia@users.noreply.github.com>
2023-05-01 18:27:44 -0300

    Update main.py
    Feito melhoria no codigo

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
+
 import tkinter
 from tkinter import messagebox
 
 count = 0
 
-msg_box = messagebox.showwarning("TOMO PAPUDO!", "VOCÊ FOI HACKEADO")
+msg_box = messagebox.showwarning("TOMOU GATINHA!", "VOCÊ FOI HACKEADO")
 
 if msg_box == 'ok':
     msg_box = messagebox.showwarning("PERA AI!", "PARA SER DESHACKEADO PRECISO QUE ME RESPONDA UMA PERGUNTA...")
@@ -16,8 +17,8 @@ while msg_box == 'no':
     count += 1
     msg_box = messagebox.askquestion("O QUE ACHA?", "ACEITA NAMORAR COMIGO?")
     if (count == 5):
-        msg_box = messagebox.showerror("TO INDO AI!", "SE VAI APANHA FEIO!")
+        msg_box = messagebox.showerror("TO INDO AI!", "VOCE VAI APANHA FEIO!")
         break
 
 if msg_box == 'yes':
-    msg_box = messagebox.showinfo("BOA!", "SABIA ESCOLHA!")
+    msg_box = messagebox.showinfo("BOA!", "SOU A MELHOR ESCOLHA!")
